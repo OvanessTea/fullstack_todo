c025f284d87b5fff6f0e61c9e931fccbfb933596
OvanessTea <OvanessTea@yandex.com>
2024-10-31 21:32:29 +0100

    update mongo schema

--- api/schemas/todo.schema.js
+++ api/schemas/todo.schema.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
 const todoSchema = new mongoose.Schema({
+    _id: String,
     title: {
         type: String,
         required: true,
     },
     description: String,
     is_done: Boolean,
+    __v: Number,
 });
 
 exports.todoSchema = todoSchema
