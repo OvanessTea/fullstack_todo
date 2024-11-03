e46c3a8fe130a325a867bc77bc531c9c4bb8388e
OvanessTea <OvanessTea@yandex.com>
2024-11-03 21:19:50 +0100

    progress designing UI

--- client/app/components/Task.tsx
+++ client/app/components/Task.tsx
@@ -9,16 +9,30 @@ interface IProp {
 
 const Task: React.FC<IProp> = (props: IProp) => {
   return (
-    <tr>
-      <td className="border border-slate-300">{props.item.title}</td>
-      <td className="border border-slate-300">{props.item.description}</td>
-      <td className="border border-slate-300 text-center">{props.item.is_done ? "True" : "False"}</td>
-      <td className="w-52 border border-slate-300">
-        <span onClick={()=>props.deleteTask(props.item._id!)} className="bg-red-500 p-2 inline-block text-white text-sm">Delete</span>
-        <Link href={`/task/edit/${props.item._id}`} className='bg-yellow-500 p-2 inline-block ml-3 text-white  text-sm'>Edit</Link>
-        <Link href={`/task/edit/${props.item._id}`} className='bg-yellow-500 p-2 inline-block ml-3 text-white  text-sm'>View</Link>
-      </td>
-    </tr>
+    <div className='border border-slate-500 rounded-lg w-60 p-2 relative'>
+      <Link 
+        href={`/task/edit/${props.item._id}`} 
+        className='text-blue-300 cursor-pointer hover:text-blue-400'
+      >
+        {props.item.title}
+      </Link>
+      <hr className='my-2' />
+      <p>{props.item.description}</p>
+      <div className='w-full fixed bottom-2'>
+        <button 
+          onClick={() => props.deleteTask(props.item._id!)} 
+          className="bg-red-500 p-2 inline-block text-white text-sm"
+        >
+          Delete
+        </button>
+        <Link 
+          href={`/task/edit/${props.item._id}`} 
+          className='bg-yellow-500 p-2 inline-block ml-3 text-white text-sm'
+        >
+          Edit
+        </Link>
+      </div>
+    </div>
   )
 }
 
