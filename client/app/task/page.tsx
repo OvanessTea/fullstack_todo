c324ea6cbcf076867eca4e7ef42f6c41101b00f0
OvanessTea <OvanessTea@yandex.com>
2024-11-06 20:07:38 +0100

    Add counting of all | closed | opened tasks Add filtering by selected tab Fix styles of tabs

--- client/app/task/page.tsx
+++ client/app/task/page.tsx
@@ -1,40 +1,55 @@
 'use client';
 import React, { useEffect, useState } from 'react'
-import { TaskModel, AddTaskModel, DeleteTaskModel, PatchTaskModel } from '../types';
+import { TaskModel } from '../types';
 import useSWR from 'swr';
 import { fetcher } from '../libs';
 import Link from 'next/link';
 import Task from '../components/Task';
 import classNames from 'classnames';
-import { usePathname } from 'next/navigation';
 
 const Tasks = () => {
     const [tasks, setTasks] = useState<TaskModel[]>([]);
     const { data, error, isLoading } = useSWR<any>(`${process.env.BASE_URL}/tasks`, fetcher);
-    console.log(data)
+    const [selectedTab, setSelectedTab] = useState<string>('All');
+
     useEffect(() => {
         if (data && data.length) {
             setTasks(data);
         }
     }, [data, isLoading])
 
+    
+    const [taskCounts, setTaskCounts] = useState<{ all: number, opened: number, closed: number }>({ all: 0, opened: 0, closed: 0 });
+    useEffect(() => {
+        if (data) {
+            const taskCounts = data.reduce((acc: { all: number, opened: number, closed: number }, task: TaskModel) => {
+                acc.all++;
+                if (task.is_done) {
+                    acc.closed++;
+                } else {
+                    acc.opened++;
+                }
+                return acc;
+            }, { all: 0, opened: 0, closed: 0 });
+            setTaskCounts(taskCounts);
+        }
+    }, [data]);
+
     if (error) return <div>Failed to load</div>;
     if (isLoading) return <div><span>Loading...</span></div>;
     if (!data) return null;
-    let delete_Task: DeleteTaskModel = async (id: string) => {
-        const res = await fetch(`${process.env.BASE_URL}/tasks/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        });
-        const content = await res.json();
-        if (content.success > 0) {
-            setTasks(tasks?.filter((task: TaskModel) => { return task._id !== id }));
+
+    useEffect(() => {
+        let filteredTasks = [...data];
+        if (selectedTab === 'Closed') {
+            filteredTasks = data.filter((task: TaskModel) => task.is_done);
+        } else if (selectedTab === 'Opened') {
+            filteredTasks = data.filter((task: TaskModel) => !task.is_done);
         }
-    }
+        setTasks(filteredTasks);
+    }, [selectedTab]);
+
     const date = new Date();
-    const pathname = usePathname();
     return (
         <div className="w-full max-w-7xl m-auto p-10">
             <div className="flex items-center gap-x-6 mb-4">
@@ -48,14 +63,35 @@ const Tasks = () => {
                 </div>
                 <Link href={`/task/create`} className="rounded-xl bg-blue-200 p-2 inline-block text-white"><span>+</span> New Task</Link>
             </div>
-            <div>
-                <button className={classNames(pathname === '/' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800')}>All</button>
-                <button className={classNames(pathname === '/opened' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800')}>Opened</button>
-                <button className={classNames(pathname === '/closed' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800')}>Closed</button>
+            <div className='flex gap-x-2 mb-2'>
+                <button
+                    className={classNames(selectedTab === 'All' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800 flex items-center gap-x-1')}
+                    onClick={() => setSelectedTab('All')}>
+                    All 
+                    <span className='text-sm bg-blue-500 rounded-lg px-1 text-white min-w-[20px] text-center'>
+                        {taskCounts.all}
+                    </span>
+                </button>
+                <button
+                    className={classNames(selectedTab === 'Opened' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800 flex items-center gap-x-1')}
+                    onClick={() => setSelectedTab('Opened')}>
+                    Opened 
+                    <span className='text-sm bg-blue-500 rounded-lg px-1 text-white min-w-[20px] text-center'>
+                        {taskCounts.opened}
+                    </span>
+                </button>
+                <button
+                    className={classNames(selectedTab === 'Closed' ? 'text-blue-400' : 'text-gray-400', 'text-lg hover:text-blue-500 active:text-blue-800 flex items-center gap-x-1')}
+                    onClick={() => setSelectedTab('Closed')}>
+                    Closed
+                    <span className='text-sm bg-blue-500 rounded-lg px-1 text-white min-w-[20px] text-center'>
+                        {taskCounts.closed}
+                    </span>
+                </button>
             </div>
-            <div className='flex flex-wrap gap-2 mb-3 overflow-y-auto'>
+            <div className='flex flex-wrap gap-2 mb-3 max-h-[650px] overflow-y-auto'>
                 {
-                    tasks && tasks.map((item: TaskModel) => <Task key={item._id} item={item} deleteTask={delete_Task} />)
+                    tasks && tasks.map((item: TaskModel) => <Task key={item._id} item={item} />)
                 }
             </div>
         </div>
