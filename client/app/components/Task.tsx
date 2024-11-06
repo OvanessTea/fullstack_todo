3dca19273a35bb90299c3b34038824dcee1823c1
OvanessTea <OvanessTea@yandex.com>
2024-11-06 20:06:36 +0100

    move Delete | Patch btns into Read page

--- client/app/components/Task.tsx
+++ client/app/components/Task.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import { DeleteTaskModel, TaskModel } from '../types';
+import { TaskModel } from '../types';
 import Link from 'next/link';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component
-import { faTrash, faGear } from "@fortawesome/free-solid-svg-icons";
 import classNames from 'classnames';
 import styles from './Task.module.scss';
 
 interface IProp {
   item: TaskModel;
-  deleteTask: DeleteTaskModel
 }
 
 const Task: React.FC<IProp> = (props: IProp) => {
@@ -16,25 +13,11 @@ const Task: React.FC<IProp> = (props: IProp) => {
     <div className={classNames('border border-slate-500 rounded-lg   p-2 pb-12 relative', styles.task_size)}>
       <div className='flex justify-between items-center'>
         <Link
-          href={`/task/edit/${props.item._id}`}
-          className='text-blue-300 cursor-pointer hover:text-blue-400'
+          href={`/task/read/${props.item._id}`}
+          className={classNames(props.item.is_done && styles.complited, 'text-xl font-bold cursor-pointer hover:text-gray-400')}
         >
           {props.item.title}
         </Link>
-        <div className='flex gap-x-2'>
-          <Link
-            href={`/task/edit/${props.item._id}`}
-            className=''
-          >
-            <FontAwesomeIcon style={{ fontSize: "16px", color: "#000000", cursor: "pointer" }} icon={faGear}></FontAwesomeIcon>
-          </Link>
-          <button
-            onClick={() => props.deleteTask(props.item._id!)}
-            className=""
-          >
-            <FontAwesomeIcon style={{ fontSize: "16px", color: "#ff0000", cursor: "pointer" }} icon={faTrash}></FontAwesomeIcon>
-          </button>
-        </div>
       </div>
       <hr className='my-2' />
       <p className='text-sm'>{props.item.description}</p>
--- client/app/task/read/[id]/page.tsx
+++ client/app/task/read/[id]/page.tsx
@@ -1,19 +1,52 @@
+'use client'
 import { fetcher } from '@/app/libs'
+import { DeleteTaskModel, TaskModel } from '@/app/types'
+import { faGear, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 import useSWR from 'swr'
 
 const Detail = ({ params }: { params: { id: string } }) => {
-    const { data: task, isLoading, error } = useSWR(`${process.env.BASE_URL}/tasks/${params.id}`, fetcher)
-
+    const { data: task, isLoading, error } = useSWR(`${process.env.BASE_URL}/tasks/${params.id}`, fetcher);
+    const router = useRouter();
     if (error) return <div>Failed to load</div>;
     if (isLoading) return <div><span>Loading...</span></div>;
     if (!task) return null;
 
+    let delete_Task: DeleteTaskModel = async (id: string) => {
+        const res = await fetch(`${process.env.BASE_URL}/tasks/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+        const content = await res.json();
+        if (content.success > 0) {
+            router.push('/');
+        }
+    }
+
     return (
         <div className='w-full'>
-            <h2 className='text-center font-bold text-3xl py-3'>{task.result.title}</h2>
+            <h2 className='text-center font-bold text-3xl py-3'>{task.title}</h2>
             <div className='w-full max-w-4xl m-auto border-[1px] p-3 border-gray-500 rounded-md'>
-                <p dangerouslySetInnerHTML={{ __html: task.result.content }}></p>
+                <p dangerouslySetInnerHTML={{ __html: task.description }}></p>
+            </div>
+            <div className='flex gap-x-2'>
+                <Link
+                    href={`/task/edit/${task._id}`}
+                    className=''
+                >
+                    <FontAwesomeIcon style={{ fontSize: "16px", color: "#000000", cursor: "pointer" }} icon={faGear}></FontAwesomeIcon>
+                </Link>
+                <button
+                    onClick={() => delete_Task(task._id!)}
+                    className=""
+                >
+                    <FontAwesomeIcon style={{ fontSize: "16px", color: "#ff0000", cursor: "pointer" }} icon={faTrash}></FontAwesomeIcon>
+                </button>
             </div>
         </div>
     )
